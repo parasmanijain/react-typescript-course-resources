6f6919f8e06fd377038562a72db900459386a19d
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-09 15:15:37 +0530

    code updated

--- attachments/03 React TypeScript Essentials/06 State/src/App.tsx
+++ attachments/03 React TypeScript Essentials/06 State/src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-
-import CourseGoal from './components/CourseGoal.tsx';
-import Header from './components/Header.tsx';
-import goalsImg from './assets/goals.jpg';
+import { useState } from "react";
+import goalsImg from "./assets/goals.jpg";
+import { Header } from "./components/Header";
+import { CourseGoal } from "./components/CourseGoal";
 
 type CourseGoal = {
   title: string;
@@ -10,15 +9,15 @@ type CourseGoal = {
   id: number;
 };
 
-export default function App() {
+export const App = () => {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
   function handleAddGoal() {
     setGoals((prevGoals) => {
       const newGoal: CourseGoal = {
         id: Math.random(),
-        title: 'Learn React + TS',
-        description: 'Learn it in depth!',
+        title: "Learn React + TS",
+        description: "Learn it in depth!",
       };
       return [...prevGoals, newGoal];
     });
@@ -26,7 +25,7 @@ export default function App() {
 
   return (
     <main>
-      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
@@ -41,4 +40,4 @@ export default function App() {
       </ul>
     </main>
   );
-}
+};
