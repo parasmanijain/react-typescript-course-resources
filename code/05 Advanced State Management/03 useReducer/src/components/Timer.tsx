1ea46dcd28a30095e2002c780ff2ab342a54a57b
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-09 14:46:40 +0530

    code updated

--- code/05 Advanced State Management/03 useReducer/src/components/Timer.tsx
+++
@@ -1,9 +0,0 @@
-import Container from './UI/Container.tsx';
-
-export default function Timer() {
-  return (
-    <Container as="article">
-      <h2>TODO: TIMER NAME</h2>
-    </Container>
-  );
-}
--- code/05 Advanced State Management/03 useReducer/src/components/Timers.tsx
+++
@@ -1,3 +0,0 @@
-export default function Timers() {
-  return <ul>{/* TODO ... */}</ul>;
-}
