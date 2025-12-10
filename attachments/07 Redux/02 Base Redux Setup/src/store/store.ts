43c73129c486093e8ed72bddbdf9d2b8708e2e98
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-10 21:18:27 +0530

    code updated

--- attachments/07 Redux/02 Base Redux Setup/src/store/store.ts
+++ attachments/07 Redux/02 Base Redux Setup/src/store/store.ts
@@ -1,9 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import { cartSlice } from './cart-slice.ts';
+import { configureStore } from "@reduxjs/toolkit";
+import { cartSlice } from "./cart-slice.ts";
 
 configureStore({
   reducer: {
-    cart: cartSlice.reducer
-  }
+    cart: cartSlice.reducer,
+  },
 });
